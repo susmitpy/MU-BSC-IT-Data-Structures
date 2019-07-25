d0637ad5ef938c35aa1ae1621dd099625ed0dd12
susmitvengurlekar <susmit.py@gmail.com>
2019-07-25 22:12:55 +0530

    self

--- dll.c
+++ dll.c
@@ -1,160 +1,114 @@
 #include <stdio.h>
-#include <conio.h>
 #include <stdlib.h>
 
 struct Node
 {
-	int data;
-	struct Node* next;
-	struct Node* prev;
+  int data;
+  struct Node* prev;
+  struct Node* next;
 };
 
 struct Node* head = NULL;
 struct Node* last = NULL;
 
-void create(int d)
+void insertNode()
 {
-	struct Node* nn;
-	nn = (struct Node*) malloc(sizeof(struct Node));
-	nn->data = d;
-	if (head == NULL)
-	{
-	  nn->next=NULL;
-	  nn->prev=NULL;
-	  head=nn;
-	  last=nn;
-	  printf("Linked List created\n");
-	}
-	else{
-		printf("Linked List already exists!\n");
-	}
+  int data;
+  printf("Enter data: ");
+  scanf("%d", &data);
+  struct Node* newNode = (struct Node*) malloc(sizeof(struct Node));
+  newNode->data = data;
+  newNode->prev = last;
+  last->next = newNode;
+  last = newNode;
+  printf("\nNode inserted\n");
 }
 
-void insertAtStart(int d)
-{
-	struct Node* nn;
-	nn = (struct Node*) malloc(sizeof(struct Node));
-	nn->data = d;
-	if (head != NULL)
-	{
-	  nn->next=head;
-	  nn->prev=NULL;
-	  head->prev=NULL;
-	  head=nn;
-	  printf("Node added\n");
-	}
-	else{
-		printf("Linked List does not exists!\n");
-	}
+void createList(int n) {
+    int i, data;
+    if (head == NULL)
+    {
+      printf("\nEnter data of first node: ");
+      scanf("%d",&data);
+      head = (struct Node*) malloc(sizeof(struct Node));
+      head->data=data;
+      head->prev=NULL;
+      head->next=NULL;
+      last=head;
+      n=n-1;
+    }
+    for(i=1;i<=n;i++)
+    {
+      insertNode();
+    }
 }
 
-
-int insertAtLast(int d)
+void displayLR()
 {
-	struct Node* nn;
-	nn = (struct Node*) malloc(sizeof(struct Node));
-	if (nn==NULL)
-	{
-		printf("Node cannot be created\n");
-		return 1;
-	}
-	nn->data = d;
-	nn->next=NULL;
-	if (head != NULL)
-	{
-		nn->prev=last;
-		last->next=nn;
-		last=nn;
-		printf("Node added\n");
-	}
-	else{
-		printf("Linked List does not exists!\n");
-	}
-	return 0;
-}
-
-void reverse()
-{
-
-  struct Node* cn;
-  struct Node* pn;
-  printf("Reversing\n");
-  last=head;
-  pn = head;
-  cn = head->next;
-  head = head->next;
-  pn->next = NULL;
-  pn->prev=cn;
-  while (head!=NULL) {
-    head = head->next;
-    cn->next = pn;
-    pn->prev=cn;
-    pn = cn;
-    cn=head;
+  struct Node* cn = head;
+  while (cn!=NULL) {
+    printf("%d ",cn->data);
+    cn = cn->next;
   }
-  head = pn;
+  printf("\n");
+}
 
-
+void displayRL()
+{
+  struct Node* cn = last;
+  while (cn!=NULL) {
+    printf("%d ",cn->data);
+    cn = cn->prev;
+  }
+  printf("\n");
 }
 
 
-void search(int data)
+
+
+
+int main()
 {
-  struct Node* curr = head;
-  int index=0;
-  printf("Searching for %d\n",data);
-  while (curr!=NULL && curr->data!=data) {
-     index++;
-    curr = curr->next;
-  }
+  int n;
+  printf("How many elements: ");
+  scanf("%d",&n);
+  createList(n);
+  printf("Display LR: ");
+  displayLR();
+  printf("Display RL: ");
+  displayRL();
+}
   printf("Position:%d\n",index+1);
 }
 
 void delete()
 {
-	struct Node* toDel = head;
-	head = head->next;
-	head->prev=NULL;
-	printf("Data: %d\n",toDel->data);
-	free(toDel);
+  struct Node* toDel = head;
+  head = head->next;
+  free(toDel);
 }
 
-
-void display()
+void reverse()
 {
-	struct Node* t;
-	printf("Displaying linked list:\n");
-	if (head != NULL)
-	{
-		t = head;
-		while(t!=NULL){
-			printf("%d ", t->data);
-			t = t->next;
-		}
-		printf("\n");
-	}
-	else{
-		printf("Linked List does not exist");
-	}
+  struct Node* cn;
+    printf("%d ",curr->data);
+    curr = curr->next;
+  }
+  printf("\n");
 }
 
 int main()
-{
-	int i,n;
-	printf("Enter data of first node: ");
-	scanf("%d", &n);
-	create(n);
-	display();
-	insertAtStart(10);
-	insertAtStart(20);
-	display();
-	insertAtLast(30);
-	display();
-	reverse();
-	display();
-	search(10);
-	delete();
-	display();
-	getch();
-	return 0;
+  insert(30);
+  printf("Searching for 20\n");
+  search(20);
+  display();
+  printf("\nReversing\n");
+  reverse();
+  display();
+  delete();
+  display();
+  delete();
+  display();
+
+  return 0;
 }
