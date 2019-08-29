2fe65b2c09082b2abcefda9c69da89b1700d5d10
Susmit Vengurlekar <susmit.py@gmail.com>
2019-08-29 15:46:37 +0530

    merge

--- merge_arrs_sort.c
+++ merge_arrs_sort.c
@@ -1,82 +1,73 @@
 #include <stdio.h>
-#include "stdlib.h"
 
-void printArray(int *arr,int n)
-{
+int n, arr[50];
+
+void getElems(){
   int i;
-  for (i=0; i < n; i++){
+  printf("Size of array: ");
+  scanf("%d", &n);
+  for (i = 0; i < n; i++){
+    printf("Elem: ");
+    scanf("%d", &arr[50]);
+  }
+}
+
+void display(){
+  int i;
+  for (i=0;i<n;i++){
     printf("%d ",arr[i]);
   }
-  printf("\n");
 }
 
-int* mergeSort(int *a1, int n1, int *a2, int n2)
-{
-  int i1, i2, j, k;
-  int* a3 = malloc(sizeof(int)*(n1+n2));
-  i1 = 0;
-  i2 = 0;
-  for(int j = 0; j<n1+n2; j++){
-    if (i1>=n1 && i2<=n2)
-    {
-      while (i2<n2) {
-        a3[j] = a2[i2];
-        i2 += 1;
-        j += 1;
-      }
-      break;
-    }
-    else if (i1<=n1 && i2>=n2)
-    {
-      while (i1<n1) {
-        a3[j] = a1[i1];
-        i1 += 1;
-        j += 1;
-      }
-      break;
-    }
-    if (a1[i1] < a2[i2]){
-      a3[j] = a1[i1];
-      i1 += 1;
+void merge_sort(int list[], int low, int mid, int high){
+  int i,k,lo,mi,temp[50];
+  lo = low;
+  i = lo;
+
+  while ((lo <= mid) && (mi <= high)){
+    if (list[lo] <= list[mi]){
+      temp[i] = list[lo];
+      lo++;
     }
     else{
-      a3[j] = a2[i2];
-      i2+=1;
+      temp[i] = list[mi];
+      mi++;
+    }
+    i++;
+  }
+  if (lo > mid){
+    for (k=mi;k<=high;k++){
+      temp[i] = list[k];
+      i++;
     }
   }
-  return a3;
-}
-
-int* getElems(int n)
-{
-  int *a = malloc(sizeof(int) * n);
-  int i;
-  printf("Enter elements: ");
-  for (i=0; i<n; i++){
-    scanf("%d", &a[i]);
+  else{
+    for (k=lo;k<=mid;k++){
+      temp[i] = list[k];
+      i++;
+    }
   }
-  return a;
+  for(k=low;k<=high;k++){
+    arr[k] = temp[k];
+  }
+
 }
 
-int getSize()
-{
-  int n;
-  printf("Enter size of array: ");
-  scanf("%d",&n);
-  return n;
+void partition(int list[], int low, int high){
+  int mid;
+  if (low <= high){
+    mid = (low+high)/2;
+    partition(list,low,mid);
+    partition(list,mid+1,high);
+    merge_sort(list,low,mid,high)
+  }
 }
 
 int main(){
-  int *a1, *a2, *a3;
-  int n1, n2;
-  printf("Array 1: ");
-  n1 = getSize();
-  a1 = getElems(n1);
-  printf("Array 2: ");
-  n2 = getSize();
-  a2 = getElems(n2);
-  a3 = mergeSort(a1,n1,a2,n2);
-  printf("Merged and sorted: ");
-  printArray(a3,n1+n2);
-  return 0;
+  getElems();
+  printf("Elements: ");
+  display();
+  partition();
+  printf("\nAfter Sorting: ");
+  display();
 }
